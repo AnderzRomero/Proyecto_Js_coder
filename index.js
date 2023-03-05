f148985a3fa911e5e8c6ce5c6fb3c8f0603ae3f7
Anderson Romero <andersonromerorico@gmail.com>
2023-03-05 00:41:13 -0500

    Se empieza a implementar el simulador de divisas

--- index.js
+++ index.js
@@ -1,14 +1,95 @@
-let numero = parseInt(prompt("Ingrese numero"));
-let saludo = prompt("Saludo");
-let numero2 = prompt("ingrese otro numero");
+//Simulador de convertidor de divisas
 
+/* 
+cada moneda 
+{
+    nombre
+    id
+    valor
+} 
+*/
 
-if ((numero > 1000) && (saludo === "hola") && ((numero2>10)&&(numero2<50))) {
-    alert("El numero ingresado es mayor de 10000 ");
-    console.log( saludo + " Bienvenido por consola");
-    alert("El numero se encuentra entre 10 y 50 ");
+// Crear Clase modena
 
+class Moneda {
+    constructor(nombre, id, valor) {
+        this.nombre = nombre
+        this.id = id
+        this.valor = valor
+    }
 }
-else{
-    alert("Datos Invalidos");
+
+const dolar = new Moneda('Dolares Americanos', 'USD', 4788)
+const euro = new Moneda('Euros', 'EUR', 5100)
+const libras = new Moneda('Libras Esterlinas', 'GBP', 5763)
+const yenes = new Moneda('Yenes Japoneses', 'JPY', 35)
+const pesos = new Moneda('Pesos Mexicanos', 'MXN', 266)
+
+let from = parseInt(prompt("Escoge la moneda que desea convertir  1. USD - 2. EUR - 3. GBP - 4. JPY - 5. MXN"));
+
+//variable para condicion de moneda correcta
+let escogioMoneda = false
+
+let infMonedaEscogida
+
+while (escogioMoneda === false) {
+    if (from === 1) {
+        escogioMoneda = true
+        infMonedaEscogida = dolar
+    } else if (from === 2) {
+        escogioMoneda = true
+        infMonedaEscogida = euro
+    } else if (from === 3) {
+        escogioMoneda = true
+        infMonedaEscogida = libras
+    } else if (from === 4) {
+        escogioMoneda = true
+        infMonedaEscogida = yenes
+    } else if (from === 5) {
+        escogioMoneda = true
+        infMonedaEscogida = pesos
+    } else {
+        from = parseInt(
+            prompt(
+                "Escoge UNA moneda correcta  1. USD - 2. EUR - 3. GBP - 4. JPY - 5. MXN"
+            ));
+    }
 }
+alert('Divisa escogida es:  '+ infMonedaEscogida.nombre +" " +infMonedaEscogida.id);
+console.log('infoMoneda', infMonedaEscogida);
+
+let to = parseInt(prompt("convertir A: 1. USD - 2. EUR - 3. GBP - 4. JPY - 5. MXN"));
+
+let escogidaConvertir = false
+let infMonedaConvertir
+
+while (escogidaConvertir === false) {
+    if (to === 1) {
+        escogidaConvertir = true
+        infMonedaConvertir = dolar
+    } else if (to === 2) {
+        escogidaConvertir = true
+        infMonedaConvertir = euro
+    } else if (to === 3) {
+        escogidaConvertir = true
+        infMonedaConvertir = libras
+    } else if (to === 4) {
+        escogidaConvertir = true
+        infMonedaConvertir = yenes
+    } else if (to === 5) {
+        escogidaConvertir = true
+        infMonedaConvertir = pesos
+    } else {
+        to = parseInt(
+            prompt(
+                "Escoge UNA moneda correcta  1. USD - 2. EUR - 3. GBP - 4. JPY - 5. MXN"
+            ));
+    }
+}
+
+const cantidad = parseFloat(prompt("Ingrese la cantidad de dinero a convertir"));
+
+
+// alert('Divisa escogida es:  '+ infMonedaConvertir.nombre +" " +infMonedaConvertir.id);
+console.log('infoMoneda', infMonedaConvertir, cantidad);
+
