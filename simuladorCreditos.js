c3f1ecb8f6b2865cc552fa69511aa59060150827
Anderson Romero <andersonromerorico@gmail.com>
2023-03-19 01:15:41 -0500

    Creador el Simulador de Creditos

--- simuladorCreditos.js
+++ simuladorCreditos.js
@@ -1,7 +1,25 @@
-const persona1 = {
-    nombre: "Anderson",
-    edad: 31,
-    calle: "carrera 7 # 45-20"
+//Simulador de Creditos Bancarios
+
+function calcularPagosMensuales(monto, tasaInteresAnual, plazoEnMeses) {
+    // Convertimos la tasa de interés anual a una tasa de interés mensual decimal
+    const tasaInteresMensual = (tasaInteresAnual / 12) / 100;
+
+    // Calculamos el pago mensual utilizando la fórmula de pagos uniformes
+    const numerador = monto * tasaInteresMensual * Math.pow(1 + tasaInteresMensual, plazoEnMeses);
+    const denominador = Math.pow(1 + tasaInteresMensual, plazoEnMeses) - 1;
+    const pagoMensual = numerador / denominador;
+    console.log(pagoMensual, "Precio mensual")
+
+    // Redondeamos el pago mensual a dos decimales
+    return Math.round(pagoMensual * 100) / 100;
 }
 
-console.log(persona1["calle"]);
+//Solicitamos los datos necesarios para realizar la simulacion del credito
+let montoPrestamo = parseFloat(prompt("Ingrese el monto de dinero que necesitas"));
+let plazoEnMeses = parseInt(prompt("Ingrese ¿A cuantos meses?"));
+const tasaInteresAnual = 12;
+
+
+const pagoMensual = calcularPagosMensuales(montoPrestamo, tasaInteresAnual, plazoEnMeses);
+
+alert(`El pago mensual es de ${pagoMensual} pesos.`);
